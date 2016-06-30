6efbc0a0acc06f440fcf3dc2a522778317a9f3e6
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-06-30 15:59:11 -0700

    Added the ability to view a list of all projects

--- Gemfile
+++ Gemfile
@@ -26,4 +26,5 @@ end
 
 group :test do
   gem 'capybara', '~> 2.7', '>= 2.7.1'
+  gem 'factory_girl_rails', '~> 4.7'
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-
+    @projects = Project.all.order('created_at DESC')
   end
   def show
     @project = Project.find(params[:id])
