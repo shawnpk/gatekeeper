d6c8eef969b487efb7a8d53813f8a73b308c36fc
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-07-02 11:35:10 -0700

    Added ability for projects to be deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,6 +33,12 @@ class ProjectsController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = 'Project has been deleted.'
+    redirect_to projects_path
+  end
 
   private
     def project_params
