06c5fa2d7a89109f7cb0766dbfc4b5130c7a6635
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-07-03 10:11:38 -0700

    Added ability for only admins to create or delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,29 +1,14 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update]
 
   def index
     @projects = Project.all.order('created_at DESC')
   end
   def show
   end
-  def new
-    @project = Project.new
-  end
   def edit
   end
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      flash[:notice] = 'Project has been created.'
-      redirect_to @project
-    else
-      flash.now[:alert] = 'Project has not been created.'
-      render :new
-    end
-  end
   def update
-
     if @project.update(project_params)
       flash[:notice] = 'Project has been updated.'
       redirect_to @project
@@ -31,12 +16,6 @@ class ProjectsController < ApplicationController
       flash[:alert] = 'Project has not been updated.'
       render :edit
     end
-  end
-  def destroy
-    @project.destroy
-
-    flash[:notice] = 'Project has been deleted.'
-    redirect_to projects_path
   end
 
   private
