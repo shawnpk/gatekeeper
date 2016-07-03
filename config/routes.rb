fbafa3058a1830a0ffeb24215deab00dbd2eb2d4
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-07-03 09:39:45 -0700

    Added admin namespace with application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'application#index', via: :get
+  end
+
   devise_for :users
+
   root 'projects#index', via: :get
+
   resources :projects do
     resources :tickets
   end
